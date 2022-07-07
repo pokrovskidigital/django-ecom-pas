298d7fbe2f720715aface9c8d2aeab37e48fee4d
nick <mr.niki002@gmail.com>
2022-07-07 16:10:00 +0500

    CategoriesListApiVIEW

--- djangoapp/starterkit/api/v1/products/services.py
+++ djangoapp/starterkit/api/v1/products/services.py
@@ -1,7 +1,7 @@
 from mptt.forms import TreeNodeChoiceField
 import django_filters
 
-from .models import Product, Category
+from .models import Product, Category, Color, Leftover
 
 
 class TreeNodeChoiceFilter(django_filters.ModelChoiceFilter):
@@ -16,8 +16,14 @@ class TreeNodeChoiceFilter(django_filters.ModelChoiceFilter):
 
 
 class ProductFilterset(django_filters.FilterSet):
-    category = TreeNodeChoiceFilter(queryset=Category.objects.all(), field_name='category__title')
+    # category = TreeNodeChoiceFilter(queryset=Category.objects.all(), field_name='category__title')
+    price = django_filters.RangeFilter(field_name='price')
+    brand = django_filters.CharFilter(field_name="brand__slug", lookup_expr="icontains")
+    color = django_filters.ModelMultipleChoiceFilter(field_name="color__code_1c", to_field_name="code_1c",
+                                                     queryset=Color.objects.all())
+    size = django_filters.ModelMultipleChoiceFilter(field_name="leftovers__size__slug", to_field_name='slug',
+                                                    queryset=Leftover.objects.all())
 
     class Meta:
         model = Product
-        fields = ('category',)
+        fields = ('price', 'brand', 'color', 'size')
