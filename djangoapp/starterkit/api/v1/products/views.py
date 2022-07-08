252d4f57067220b87cdf50fb72afee715f1f1c85
nick <mr.niki002@gmail.com>
2022-07-08 11:16:46 +0500

    OptionCategoryView

--- djangoapp/starterkit/api/v1/products/views.py
+++ djangoapp/starterkit/api/v1/products/views.py
@@ -60,7 +60,7 @@ class OptionCategoryView(APIView):
         options_dict = {'sizes': [], 'colors': [],
                         'max_price': 0, 'min_price': 0}
         for color in products.values_list('color__title', flat=True):
-            if color not in options_dict['colors']:
+            if color not in options_dict['colors'] and color is not None:
                 options_dict['colors'].append(color)
         for size in products.values_list('leftovers__parent_size__title', flat=True):
             if size not in options_dict['sizes']:
