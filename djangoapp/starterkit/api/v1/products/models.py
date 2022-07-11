a09e5d97f769b65ff0df317d2f7bfb644ed8e2a0
OtsoSilver <mr.niki002@gmail.com>
2022-07-11 11:51:12 +0300

    MainPage

--- djangoapp/starterkit/api/v1/products/models.py
+++ djangoapp/starterkit/api/v1/products/models.py
@@ -179,7 +179,7 @@ class Options(models.Model):
 class Compilation(models.Model):
     title = models.CharField(max_length=200, verbose_name="title")
     subtitle = models.CharField(max_length=200, verbose_name="subtitle")
-    sex = models.ForeignKey('Sex', on_delete=models.PROTECT, null=True, blank=True)
+    sex = models.ForeignKey('Sex', on_delete=models.PROTECT, blank=True)
     text = models.TextField(max_length=1000, verbose_name="text")
     title_image = models.ForeignKey('Image', on_delete=models.PROTECT, null=True, blank=True)
     products = models.ManyToManyField('Product', null=True, blank=True, related_name="Compilations")
