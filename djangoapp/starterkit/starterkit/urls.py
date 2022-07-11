72012e9f86d0cd847590ea8c0cb46298536f9ca1
OtsoSilver <mr.niki002@gmail.com>
2022-07-11 12:01:58 +0300

    MainPage

--- djangoapp/starterkit/starterkit/urls.py
+++ djangoapp/starterkit/starterkit/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/catalog/', include('api.v1.products.urls')),
     path('api/menu/', include('api.v1.menu.urls')),
-    re_path(r"^swagger(?P\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
