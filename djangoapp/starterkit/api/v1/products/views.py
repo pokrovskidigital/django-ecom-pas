608d9265181b03229bb2d910385d6a31ad43e30a
OtsoSilver <mr.niki002@gmail.com>
2022-07-15 09:19:35 +0300

    BrandV

--- djangoapp/starterkit/api/v1/products/views.py
+++ djangoapp/starterkit/api/v1/products/views.py
@@ -70,6 +70,17 @@ class ProductsSearchListApiView(mixins.ListModelMixin, GenericAPIView):
     serializer_class = ProductsViewSerializer
     permission_classes = (AllowAny,)
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        print(queryset)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
     def post(self, request, *args, **kwargs):
         return self.list(request)
 
