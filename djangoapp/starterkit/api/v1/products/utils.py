fafc2577e13619ed4074286f74cca832dc312066
SilentLords <41440891+SilentLords@users.noreply.github.com>
2022-07-19 11:17:48 +0500

    Menu

--- djangoapp/starterkit/api/v1/products/utils.py
+++ djangoapp/starterkit/api/v1/products/utils.py
@@ -7,6 +7,7 @@ def create_recomendations():
         recom_products = prods.filter(price__gt=(prod.price - prod.price * 0.3),
                                       price__lt=(prod.price + prod.price * 0.3),
                                       category=prod.category)
-        prod.similar_products.add(*recom_products[:5].values('pk')['pk'])
+        print(recom_products[:5].values('pk'))
+        # prod.similar_products.add(*(recom_products[:5].values('pk'))['pk'])
         prod.save()
         print(prod.similar_products.all())
