fd0596ef046c3f53a96d71498aecf2e7e9db96da
root <root@vm-feea529e.na4u.ru>
2022-07-19 07:04:30 +0000

    asda

--- djangoapp/starterkit/api/v1/products/utils.py
+++ djangoapp/starterkit/api/v1/products/utils.py
@@ -7,10 +7,13 @@ def create_recomendations():
         recom_products = prods.filter(price__gt=(prod.price - prod.price * 0.3),
                                       price__lt=(prod.price + prod.price * 0.3),
                                       category=prod.category)
-        try:
-            print(*recom_products[:5].values('pk')['pk'])
-        except:
-            print('empty')
-        # prod.similar_products.add(*(recom_products[:5].values('pk'))['pk'])
+        for pp in prod.similar_products.all():
+            prod.similar_products.remove(pp)
+        print(*(recom_products[:5].values_list('pk')))
+        for p in recom_products[:5].values_list('pk'):
+            try:
+                prod.similar_products.add(p[0])
+            except:
+                print('empty')
         prod.save()
         print(prod.similar_products.all())
