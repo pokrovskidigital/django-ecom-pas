6df56eefbc9caed60b19dd3ddabf7cd122d8aa91
SilentLords <41440891+SilentLords@users.noreply.github.com>
2022-07-19 10:52:11 +0500

    Menu

--- djangoapp/starterkit/api/v1/products/utils.py
+++ djangoapp/starterkit/api/v1/products/utils.py
@@ -1,28 +1,4 @@
-from .models import Product
-
-
-def brand_image_directory_path(instance, filename):
-    return f'brand_{instance.title}/image/{filename}'
-
-
-def brand_icon_directory_path(instance, filename):
-    return f'brand_{instance.title}/icon/{filename}'
-
-
-def product_image_directory_path(instance, filename):
-    return f'product_{instance.title}/image/{filename}'
-
-
-def l_image_directory_path(instance, filename):
-    return f'images/{instance.type}/{instance.title}/l/{filename}'
-
-
-def s_image_directory_path(instance, filename):
-    return f'images/{instance.type}/{instance.title}/s/{filename}'
-
-
-def m_image_directory_path(instance, filename):
-    return f'images/{instance.type}/{instance.title}/m/{filename}'
+from api.v1.products.models import Product
 
 
 def create_recomendations():
