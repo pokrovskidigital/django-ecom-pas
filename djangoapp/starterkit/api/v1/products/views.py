0642164db02f6e9a0f9f027bacbb2fbbaacbc9ec
SilentLords <41440891+SilentLords@users.noreply.github.com>
2022-07-20 14:44:35 +0500

    Menu

--- djangoapp/starterkit/api/v1/products/views.py
+++ djangoapp/starterkit/api/v1/products/views.py
@@ -149,6 +149,8 @@ def get_options(options_dict, products):
     for brand in products.values_list('brand__title', 'brand__slug').distinct():
         if brand not in options_dict['brands']:
             options_dict['brands'].append(brand)
+    cats = products.values_list('category_pk')
+    print(cats)
     options_dict['min_price'] = products.order_by('-price').first().price
     options_dict['max_price'] = products.order_by('price').first().price
     return options_dict
